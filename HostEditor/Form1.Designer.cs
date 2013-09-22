e8e0533084c93a41ef3f613c0d5f43324b5eea00
catwen <jquery.min.js@gmail.com>
2013-09-22 19:28:42 +0800

    create
    create

---
+++ HostEditor/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace HostEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelGroupBoxes = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 478);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(655, 45);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(138, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "修改";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelGroupBoxes
+            // 
+            this.panelGroupBoxes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelGroupBoxes.AutoScroll = true;
+            this.panelGroupBoxes.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelGroupBoxes.Location = new System.Drawing.Point(0, 0);
+            this.panelGroupBoxes.Margin = new System.Windows.Forms.Padding(0);
+            this.panelGroupBoxes.Name = "panelGroupBoxes";
+            this.panelGroupBoxes.Size = new System.Drawing.Size(655, 481);
+            this.panelGroupBoxes.TabIndex = 2;
+            this.panelGroupBoxes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button2.Location = new System.Drawing.Point(392, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "查看";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(655, 523);
+            this.Controls.Add(this.panelGroupBoxes);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "HostEditor";
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelGroupBoxes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HostEditor/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HostEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateGroupBox();
+        }
+
+        private static string[] GetConfigKeys()
+        {
+            if (ConfigurationSettings.AppSettings != null)
+            {
+                string[] keys = ConfigurationSettings.AppSettings.AllKeys;
+                return keys;
+            }
+            return null;
+        }
+
+        private static string[] GetConfigValuesByKey(string key)
+        {
+            string[] result = new string[] { };
+            AppSettingsReader appRead = new AppSettingsReader();
+            object value = appRead.GetValue(key, typeof(string));
+            if (value != null)
+            {
+                string values = (string)value;
+                if (values.IndexOf(",") != -1)
+                {
+                    string[] str = values.Split(',');
+                    result = str;
+                }
+                else
+                {
+                    result = new string[] { values };
+                }
+            }
+            return result;
+        }
+
+        private void CreateGroupBox()
+        {
+            const int gbHeight = 40;
+            string[] keys = GetConfigKeys();
+            for (int i = 0; i < keys.Length; i++)
+            {
+                GroupBox groubox = new GroupBox { Name = "groupbox" + i, Text = keys[i] };
+                string[] values = GetConfigValuesByKey(keys[i]);
+                int y = 0;
+                for (int j = 0; j < values.Length; j++)
+                {
+                    y += 30;
+                    CheckBox cb = CreateCheckBox(150, gbHeight, new Point(j * 150 + 50, 20), values[j], values[j] + " " + keys[i]);
+                    groubox.Controls.Add(cb);
+                }
+                panelGroupBoxes.Controls.Add(groubox);
+                panelGroupBoxes.Controls["groupbox" + i].Height = gbHeight + 50;
+                if (i == 0)
+                {
+                    panelGroupBoxes.Controls["groupbox" + i].Location = new Point(40, this.panelGroupBoxes.Controls["groupbox" + i].Height * (i + i) + 20);
+                    panelGroupBoxes.Controls["groupbox" + i].Width = panelGroupBoxes.Width - 80;
+                }
+                else
+                {
+                    panelGroupBoxes.Controls["groupbox" + i].Location = new Point(40, this.panelGroupBoxes.Controls["groupbox" + (i - 1)].Location.Y + this.panelGroupBoxes.Controls["groupbox" + (i - 1)].Height + 30);
+                    panelGroupBoxes.Controls["groupbox" + i].Width = panelGroupBoxes.Width - 80;
+                }
+            }
+        }
+
+        private static CheckBox CreateCheckBox(int width, int height, Point point, string text, string tag)
+        {
+            CheckBox cb = new CheckBox();
+            cb.CheckedChanged += (sender, args) =>
+            {
+                if (sender != null)
+                {
+                    CheckBox scb = sender as CheckBox;
+                    if (scb != null && scb.Checked)
+                    {
+                        foreach (CheckBox control in scb.Parent.Controls)
+                        {
+                            if (control != sender)
+                            {
+                                control.Checked = false;
+                            }
+                        }
+                    }
+                }
+            };
+            cb.Location = point;
+            cb.Text = text;
+            cb.Height = height;
+            cb.Width = width;
+            cb.Tag = tag;
+            return cb;
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in panelGroupBoxes.Controls)
+            {
+                if (control is GroupBox)
+                {
+                    int formWidth = panelGroupBoxes.Width;
+                    control.Width = formWidth - (control.Location.X * 2);
+                }
+            }
+        }
+
+        private void panel3_MouseClick(object sender, MouseEventArgs e)
+        {
+            //获取光标位置
+            Point mousePoint = new Point(e.X, e.Y);
+            //换算成相对本窗体的位置
+            mousePoint.Offset(Location.X, this.Location.Y);
+            //判断是否在panel内
+            if (panelGroupBoxes.RectangleToScreen(panelGroupBoxes.DisplayRectangle).Contains(mousePoint))
+            {
+                //滚动
+                panelGroupBoxes.AutoScrollPosition = new Point(0, panelGroupBoxes.VerticalScroll.Value - e.Delta);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["hostpath"].ConnectionString; //host文件路径
+            string constrCommonHost = ConfigurationManager.ConnectionStrings["commonhost"].ConnectionString; //常用host路径
+            string stredit = "";
+            string str = "";
+            int count = 0;
+
+
+            if (!string.IsNullOrEmpty(constrCommonHost))
+            {
+                constrCommonHost = constrCommonHost.Trim();
+                if (constrCommonHost.IndexOf(',') != -1)
+                {
+                    string[] strings = constrCommonHost.Split(',');
+                    foreach (string s in strings)
+                    {
+                        str += (s + "\r\n");
+                    }
+                }
+                else
+                {
+                    str += (constrCommonHost.Trim() + "\r\n");
+                }
+                str += "####################\r\n";
+            }
+
+            foreach (Control control in panelGroupBoxes.Controls)
+            {
+                if (control is GroupBox)
+                {
+                    foreach (Control controlcb in control.Controls)
+                    {
+                        if (controlcb is CheckBox)
+                        {
+                            CheckBox cb = controlcb as CheckBox;
+                            if (cb.Checked)
+                            {
+                                count++;
+                                str += cb.Tag + "\r\n\r\n";
+                                stredit += cb.Tag + "\r\n\r\n";
+                            }
+                        }
+                    }
+                }
+            }
+            if (count > 0)
+            {
+                DialogResult dr = MessageBox.Show("Host文件将会被以下内容覆盖：\r\n\r\n" + stredit, "操作确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    try
+                    {
+
+                        if (!string.IsNullOrEmpty(constr))
+                        {
+                            File.WriteAllText(constr, str, Encoding.ASCII);
+                            MessageBox.Show("修改成功，重启浏览器生效", "操作提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("修改失败，未能读取到host文件路径信息", "操作提示");
+                        }
+                    }
+                    catch (Exception exx)
+                    {
+                        MessageBox.Show("修改失败", "操作提示");
+                    }
+
+                }
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("未检测到需要修改的信息，点击确定会清空当前host信息", "操作提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (dialogResult == DialogResult.OK)
+                {
+                    File.WriteAllText(constr, str, Encoding.ASCII);
+                    MessageBox.Show("清空成功，重启浏览器生效", "操作提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["hostpath"].ConnectionString;
+            if (!string.IsNullOrEmpty(constr))
+            {
+                string readAllText = File.ReadAllText(constr, Encoding.ASCII);
+                if (!string.IsNullOrEmpty(readAllText) && !string.IsNullOrEmpty(readAllText.Replace("\r\n", "").Replace(" ", "")))
+                {
+                    MessageBox.Show(readAllText, "本地host信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Host文件为空", "本地host信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+        }
+    }
+}
